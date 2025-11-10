fec0d7e480b2e0e28e841dcf1f3e822ee885fc24
HJ Na <nahj9007@gmail.com>
2025-11-10 18:45:25 +0900

    Add files via upload

---
+++ aurora-win/tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: ["class"],
+  content: [
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}',
+    './webui/**/*.{ts,tsx}' // webui 폴더 인식
+  ],
+  prefix: "",
+  theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
+    extend: {
+      // (shadcn/ui 기본 설정)
+    },
+  },
+  plugins: [require("tailwindcss-animate")],
+}
